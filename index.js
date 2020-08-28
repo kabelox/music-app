da2e5aeddfee6b000df687bc670fc42dc3de320d
Owethu99Twala <owethutwala99@gmail.com>
2020-08-28 13:55:34 +0200

    music app

---
+++ index.js
@@ -0,0 +1,33 @@
+function playj1(){
+        let lion = new Audio("http://188.138.16.150/p.php?s=eNoBQAC%252F%252F%252FIfrFznyJ6%252BVl5%252FR5PSMq%252F0ppTsCHbSrdDrPDhwi4y1iRrSm6RtsGH25AlpaMm5ZA%252B88nJFK5rURi3cA6enGDyVniI0");
+    lion.play();
+    }
+    
+    function playj2(){
+        let lion = new Audio('http://188.138.57.98/p.php?s=eNoBOADH%252F8CIYXf%252BPQFt0Nf8IgeeNGr%252F4RdevUDk33DtXpWhEyUL0dBSJR4M1aTKfkLip9osDycH6IxLVKzWKu4bwQ%253D%253D');
+    lion.play();
+    }
+    
+    function playj3(){
+        let lion = new Audio('http://62.138.24.132/p.php?s=eNoBQAC%252F%252Fx69hy6lxndaLbFgfF%252Fb4iousfC9ECc4pqk%252FAlsPuxYWysa1UkDg%252FRn3%252FvxHhgI75Fil7jlHY3F1mZmzclNYJxfe4B5J');
+    lion.play();
+    }
+    // the weekend
+
+    function playw1(){
+        let lion = new Audio("http://85.25.218.17/p.php?s=eNoBWACn%252F9OVXv0RIQgIPRjnCh8Yc%252FFJSmDw%252BKJPIc971M14WfNAFizeCsyCPKMOU3KthhdcizgumzM5w6qFJYLNLJKlFCt%252BaOgwDBPi0DImLWE4ne0npPfji26oZryi7idy");
+    lion.play();
+    }
+    
+    function playw2(){
+        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBSAC3%252F5mvVwQSbhvPUSEbg8RQZKHnnAl%252BUs3Nk7TiOmAmXawirqIBIul0VFRx8hb2lL%252FH%252FhLwuzGi5697PAeRKVhQMtjva3xd22V7Tce%252BIss%253D');
+    lion.play();
+    }
+    
+    function playw3(){
+        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBUACv%252F1gu4eE6%252B05bZN%252BSTTvB8nx%252BQcDjuq8d5DW2EqS4eT%252BShuhCRmuUxTCmvrEGMns1kKIt92f2LSmu1MvgDrxHpkgKJ4jnHb3em2EkFfjQK9l3mawpDg%253D%253D');
+    lion.play();
+    }
+    
+
+    
