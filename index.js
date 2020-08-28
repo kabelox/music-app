47b65c63064af13d77644050d1f108762a39fd39
Owethu99Twala <owethutwala99@gmail.com>
2020-08-28 17:29:58 +0200

    the music app

--- index.js
+++ index.js
@@ -1,65 +1,239 @@
-function playj1(){
-        let lion = new Audio("http://188.138.16.150/p.php?s=eNoBQAC%252F%252F9V%252BHd1B4FVWwl%252FAj5YTjcFPsOlCiagunx8ST9cdevCrBKyTEYLuXgTD%252FRHJKDkduK5oQt1nOLLq%252FMrZrD1VfnYRlCBp");
-    lion.play();
+// function playj1(){
+//     let lion = new Audio("http://188.138.16.150/p.php?s=eNoBQAC%252F%252F4%252B9%252FyYnSnfgqhRTqbH3HRc7OYNvtXZF9PDUJWIv4VHJPLJ4kqLav792lOLj36zLmcl7Ysxla1xUjJkVsbSwn3RNFSMT");
+// lion.play();
 
 
 
+function playl(){
+    let lion = new Audio('http://188.138.16.150/p.php?s=eNoBQAC%252F%252F4%252B9%252FyYnSnfgqhRTqbH3HRc7OYNvtXZF9PDUJWIv4VHJPLJ4kqLav792lOLj36zLmcl7Ysxla1xUjJkVsbSwn3RNFSMT');
+lion.play();
 }
-  
+
+function playl2(){
+    let lion = new Audio('http://188.138.57.98/p.php?s=eNoBOADH%252Fyos0AYySzK0IbqWVW0fXVBTBkVVmjZgsuuLiESNZEdXK5nWkJnm9%252BhTK%252FYOhyPIvZnOCxfkhrM0jB0ZSA%253D%253D');
+lion.play();
+}
+
+
+function playl3(){
+    let lion = new Audio('http://85.25.74.135/p.php?s=eNoBUACv%252F77o%252BBL6wRoIqKZ4Zb0f13bJpYIWxLoMguQK0cGI5GetEJFTdSkQhADmhAemKYe0l9TEPYUGoGxvcv85TV6g0FrOyBRygjaWf9mm1C0eQaB9iIgoCw%253D%253D');
+lion.play();
+}
+
+// }
+
 
 function load(){
 
-    window.location.reload(true);
+window.location.reload(true);
 }
 
 
 
-   
-    
-    
-    function playj2(){
-        let lion = new Audio('http://188.138.57.98/p.php?s=eNoBOADH%252FxC%252BfDdMjQv7raK7lzyQPl9t%252B0r8G%252B9PmE7%252B%252FQskBke%252BDbTMmX40C27qhwsIFkAOdfR%252FkF4MveZp70IZtA%253D%253D');
-    lion.play();
-    }
-    
-    function playj3(){
-        let lion = new Audio('http://85.25.74.135/p.php?s=eNoBUACv%252F%252FDEPR%252BlevqE1rexRjPNoUxOHKsNjgqled3eQXQx%252Bgs5YQkH8qYmRF0wz5BrQNFw4FZnXu8%252BSyDxPkoPT%252FAh%252BtYRlWosmTjTQKhroFXay%252FDoGCcnEQ%253D%253D');
-    lion.play();
-    }
-    // the weekend
 
-    function playw1(){
-        let lion = new Audio("https://d274.cdn-me.net/p.php?s=eNoBUACv%252F%252B5Hle%252B5FAnpUoX51AHZL9%252F3I59zxWddchzBZvy3J6cmZA65Dc1%252F8mVv%252FAJQFsArkwKg5vhEnOKDs47zuF%252Fyx1ZjB%252FAddzUSu1Qb0giq5Se9pLspDQ%253D%253D");
-    lion.play();
-    }
-    
-    function playw2(){
-        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBSAC3%252F%252BiOokUc3r7qdVF%252FcfpOTc1UIE%252FG8NNX3xNP4CbOm7M7nkSbADw1SkUQG0RJ4WBg4eG%252BUyTmfwr%252FJ3oIZ8Du3JRe6YVJfJWwzzDkJGc%253D');
-    lion.play();
-    }
-    
-    function playw3(){
-        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBWACn%252F2y29dMf0t1MUsCwe0RmoDJEOXfMjOEkeSXXkM5y24b4rRCdBRq4rbym8Wc5wgA4idJiyXWFZ6r%252FmN8nhyGQiTss3JlHiugpUP%252F00aYSx18VIFQju0A9TV0axSyl');
-    lion.play();
-    }
 
-    // usher
 
-    function playu1(){
-        let lion = new Audio("https://d274.cdn-me.net/p.php?s=eNoBOADH%252Fz4S%252FRYEjRapdDsw%252BHvPpsBAfOYIKXa4wSwIOHnQt%252BkBYzpMNUvYso8H08%252FIoPLAU2iZqn4lev%252FSznMbhA%253D%253D");
-    lion.play();
-    }
-    
-    function playu2(){
-        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBMADP%252F4t18jdYD%252FPjmTbAlDdu5U2BcHef7tXfR0C88BghKotEwI9Fl6WQgLLX4M8Pqrcm5nIuGgM%253D');
-    lion.play();
-    }
-    
-    function playu3(){
-        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBOADH%252F5%252FgxdC52qdYIgleXalYi%252FiIxfkljPafOUAq5E7frgf9JOg6QoJ14mbrZ6u873N6pobMTXtnDQx%252FgPkeHg%253D%253D');
-    lion.play();
-    }
-    
-    
+// function playj2(){
+//     let lion = new Audio('http://188.138.57.98/p.php?s=eNoBOADH%252FxC%252BfDdMjQv7raK7lzyQPl9t%252B0r8G%252B9PmE7%252B%252FQskBke%252BDbTMmX40C27qhwsIFkAOdfR%252FkF4MveZp70IZtA%253D%253D');
+// lion.play();
+// }
 
+
+function playl4(){
+    let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBSAC3%252F2lIXksOWhO2mCxkz25XuAgzQHGK1b%252FNXeN431C%252FUUXdbbQJ4pESLXLTLbDZdZcFOnT56QClPDLCTxIr%252BSdkx4q5WtyM3EhgoazHIj4%253D');
+lion.play();
+}
+
+function playl5(){
+    let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBgAB%252F%252FygdU8Y4RZtyLfrtaCHAnSGeka7zLMcAikb2hqd0GQLg39RCMdRuFQU65zDJUEXmS1bmNf5Nha0gNIN9OBFhu6ANipsBSfwPLnuM6CFAEWu%252BF7obI5hmEjB7bSRN8QDn%252BlQPkmZN9jOu4igi5gP4tmd7ZHJwCS%252B7XBmwsQkqizy2iC44bg%253D%253D');
+lion.play();
+}
+
+
+function playl6(){
+    let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBUACv%252F5HMY87B%252BgW%252FHU3n8x4yTeAomnTrQp5LAjIbq9iH7uDYYn1ianzd4VN1ic%252BIVjg8bPLum6njvt43PF7hGoHaViTmraCulXHWvBsPYVvAA4XmyjAqrg%253D%253D');
+lion.play();
+}
+
+// function playj3(){
+//     let lion = new Audio('http://85.25.74.135/p.php?s=eNoBUACv%252F%252FDEPR%252BlevqE1rexRjPNoUxOHKsNjgqled3eQXQx%252Bgs5YQkH8qYmRF0wz5BrQNFw4FZnXu8%252BSyDxPkoPT%252FAh%252BtYRlWosmTjTQKhroFXay%252FDoGCcnEQ%253D%253D');
+// lion.play();
+// }
+// // the weekend
+
+// function playw1(){
+//     let lion = new Audio("https://d274.cdn-me.net/p.php?s=eNoBUACv%252F%252B5Hle%252B5FAnpUoX51AHZL9%252F3I59zxWddchzBZvy3J6cmZA65Dc1%252F8mVv%252FAJQFsArkwKg5vhEnOKDs47zuF%252Fyx1ZjB%252FAddzUSu1Qb0giq5Se9pLspDQ%253D%253D");
+// lion.play();
+// }
+
+// function playw2(){
+//     let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBSAC3%252F%252BiOokUc3r7qdVF%252FcfpOTc1UIE%252FG8NNX3xNP4CbOm7M7nkSbADw1SkUQG0RJ4WBg4eG%252BUyTmfwr%252FJ3oIZ8Du3JRe6YVJfJWwzzDkJGc%253D');
+// lion.play();
+// }
+
+// function playw3(){
+//     let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBWACn%252F2y29dMf0t1MUsCwe0RmoDJEOXfMjOEkeSXXkM5y24b4rRCdBRq4rbym8Wc5wgA4idJiyXWFZ6r%252FmN8nhyGQiTss3JlHiugpUP%252F00aYSx18VIFQju0A9TV0axSyl');
+// lion.play();
+// }
+
+// // usher
+
+// function playu1(){
+//     let lion = new Audio("https://d274.cdn-me.net/p.php?s=eNoBOADH%252Fz4S%252FRYEjRapdDsw%252BHvPpsBAfOYIKXa4wSwIOHnQt%252BkBYzpMNUvYso8H08%252FIoPLAU2iZqn4lev%252FSznMbhA%253D%253D");
+// lion.play();
+// }
+
+
+function playl7(){
+    let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBSAC3%252F7QPhnG1B%252F5yRZYCpqKRwuMOuW0VAeFJVb%252Fb6Fp5q7h20gsH1oxBwnP5IXVLkUQbQR9ZytN8xmy%252FSMCF6ErAQDrQj1Q6RM173AnhI9A%253D');
+lion.play();
+}
+
+function playl8(){
+    let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBQAC%252F%252FxYRcdgSrat5DCS172uPanTnMRMc5I5c5oywExYVVUXqjoPvu1BH2%252FZEomeUeH3CVHx355u58lxejRtzy8%252ByW1%252FSkyAD');
+lion.play();
+}
+
+
+function playl9(){
+    let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBUACv%252F85Y%252BLiC6TFBFyapmRfZa%252B2J0t7yKvWxGSQ%252B97jerbKLaPWcAt4%252BwhUm2mAJBnC1OoIbEAsMbI0arhaXGnVrSJrXRTDDB%252F9%252FrUQaR7SaMu2ZajImHg%253D%253D');
+lion.play();
+}
+// function playu2(){
+//     let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBMADP%252F4t18jdYD%252FPjmTbAlDdu5U2BcHef7tXfR0C88BghKotEwI9Fl6WQgLLX4M8Pqrcm5nIuGgM%253D');
+// lion.play();
+// }
+
+// function playu3(){
+//     let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBOADH%252F5%252FgxdC52qdYIgleXalYi%252FiIxfkljPafOUAq5E7frgf9JOg6QoJ14mbrZ6u873N6pobMTXtnDQx%252FgPkeHg%253D%253D');
+// lion.play();
+// }
+
+
+// sound works
+
+function playl(){
+    let lion = new Audio('http://188.138.16.150/p.php?s=eNoBQAC%252F%252F4%252B9%252FyYnSnfgqhRTqbH3HRc7OYNvtXZF9PDUJWIv4VHJPLJ4kqLav792lOLj36zLmcl7Ysxla1xUjJkVsbSwn3RNFSMT');
+lion.play();
+}
+
+
+
+function playb(){
+    let buff = new Audio('http://soundfxcenter.com/animals/farm-animals/8d82b5_Buffalo_Moo_Sound_Effect.mp3');
+buff.play();
+
+
+
+}
+
+
+function playlep(){
+    let leo = new Audio('http://www.animal-sounds.org/jungle/Leopard%20growl%20animals089.wav');
+leo.play();
+ 
+}
+
+function playe(){
+
+let ele = new Audio('http://www.elemotion.org/wp-content/uploads/2011/09/roar.mp3');
+ele.play();
+
+}
+
+
+function playr(){
+    let rhino = new Audio('http://www.zar.co.za/sounds/rhino.mp3');
+rhino.play();
+
+}
+
+// hover image effect
+
+function newr(){
+    document.getElementById("ro").src="ry.jpg"
+
+
+    let rhino = new Audio('http://www.zar.co.za/sounds/rhino.mp3');
+    rhino.play();
     
+}
+
+function oldr(){
+    document.getElementById("ro").src="rino.jpg"
+}
+
+
+
+
+
+
+
+
+
+function newlep(){
+    document.getElementById("lepo").src="lepy.jpg"
+
+    let leo = new Audio('http://www.animal-sounds.org/jungle/Leopard%20growl%20animals089.wav');
+    leo.play();
+     
+}
+
+
+function oldlep(){
+    document.getElementById("lepo").src="leopard.jpg"
+}
+
+
+
+
+
+
+
+function newl(){
+    document.getElementById("lo").src="ly.jpg"
+    let lion = new Audio('http://www.acc.umu.se/~leng/elsalejon.wav');
+    lion.play();
+}
+
+
+function oldl(){
+    document.getElementById("lo").src="lio.jpg"
+}
+
+
+
+
+
+
+function newb(){
+    document.getElementById("bo").src="by.jpg"
+
+    let buff = new Audio('http://soundfxcenter.com/animals/farm-animals/8d82b5_Buffalo_Moo_Sound_Effect.mp3');
+    buff.play();
+    
+}
+
+
+
+function oldb(){
+    document.getElementById("bo").src="buffulo.jpg"
+}
+
+
+
+
+
+
+
+function newe(){
+    document.getElementById("eo").src="eley.jpg"
+    let ele = new Audio('http://www.elemotion.org/wp-content/uploads/2011/09/roar.mp3');
+ele.play();
+
+}
+
+
+function olde(){
+    document.getElementById("eo").src="ele.jpg"
+}
