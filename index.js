ee8360ca3baabf0d5583f05dada5c24241664550
Owethu99Twala <owethutwala99@gmail.com>
2020-08-28 15:58:26 +0200

    music app

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
 function playj1(){
-        let lion = new Audio("http://188.138.16.150/p.php?s=eNoBQAC%252F%252F%252FIfrFznyJ6%252BVl5%252FR5PSMq%252F0ppTsCHbSrdDrPDhwi4y1iRrSm6RtsGH25AlpaMm5ZA%252B88nJFK5rURi3cA6enGDyVniI0");
+        let lion = new Audio("http://188.138.16.150/p.php?s=eNoBQAC%252F%252F9V%252BHd1B4FVWwl%252FAj5YTjcFPsOlCiagunx8ST9cdevCrBKyTEYLuXgTD%252FRHJKDkduK5oQt1nOLLq%252FMrZrD1VfnYRlCBp");
     lion.play();
-    }
+
+
+
+}
+  
+
+function load(){
+
+    window.location.reload(true);
+}
+
+
+
+   
+    
     
     function playj2(){
-        let lion = new Audio('http://188.138.57.98/p.php?s=eNoBOADH%252F8CIYXf%252BPQFt0Nf8IgeeNGr%252F4RdevUDk33DtXpWhEyUL0dBSJR4M1aTKfkLip9osDycH6IxLVKzWKu4bwQ%253D%253D');
+        let lion = new Audio('http://188.138.57.98/p.php?s=eNoBOADH%252FxC%252BfDdMjQv7raK7lzyQPl9t%252B0r8G%252B9PmE7%252B%252FQskBke%252BDbTMmX40C27qhwsIFkAOdfR%252FkF4MveZp70IZtA%253D%253D');
     lion.play();
     }
     
     function playj3(){
-        let lion = new Audio('http://62.138.24.132/p.php?s=eNoBQAC%252F%252Fx69hy6lxndaLbFgfF%252Fb4iousfC9ECc4pqk%252FAlsPuxYWysa1UkDg%252FRn3%252FvxHhgI75Fil7jlHY3F1mZmzclNYJxfe4B5J');
+        let lion = new Audio('http://85.25.74.135/p.php?s=eNoBUACv%252F%252FDEPR%252BlevqE1rexRjPNoUxOHKsNjgqled3eQXQx%252Bgs5YQkH8qYmRF0wz5BrQNFw4FZnXu8%252BSyDxPkoPT%252FAh%252BtYRlWosmTjTQKhroFXay%252FDoGCcnEQ%253D%253D');
     lion.play();
     }
     // the weekend
 
     function playw1(){
-        let lion = new Audio("http://85.25.218.17/p.php?s=eNoBWACn%252F9OVXv0RIQgIPRjnCh8Yc%252FFJSmDw%252BKJPIc971M14WfNAFizeCsyCPKMOU3KthhdcizgumzM5w6qFJYLNLJKlFCt%252BaOgwDBPi0DImLWE4ne0npPfji26oZryi7idy");
+        let lion = new Audio("https://d274.cdn-me.net/p.php?s=eNoBUACv%252F%252B5Hle%252B5FAnpUoX51AHZL9%252F3I59zxWddchzBZvy3J6cmZA65Dc1%252F8mVv%252FAJQFsArkwKg5vhEnOKDs47zuF%252Fyx1ZjB%252FAddzUSu1Qb0giq5Se9pLspDQ%253D%253D");
     lion.play();
     }
     
     function playw2(){
-        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBSAC3%252F5mvVwQSbhvPUSEbg8RQZKHnnAl%252BUs3Nk7TiOmAmXawirqIBIul0VFRx8hb2lL%252FH%252FhLwuzGi5697PAeRKVhQMtjva3xd22V7Tce%252BIss%253D');
+        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBSAC3%252F%252BiOokUc3r7qdVF%252FcfpOTc1UIE%252FG8NNX3xNP4CbOm7M7nkSbADw1SkUQG0RJ4WBg4eG%252BUyTmfwr%252FJ3oIZ8Du3JRe6YVJfJWwzzDkJGc%253D');
     lion.play();
     }
     
     function playw3(){
-        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBUACv%252F1gu4eE6%252B05bZN%252BSTTvB8nx%252BQcDjuq8d5DW2EqS4eT%252BShuhCRmuUxTCmvrEGMns1kKIt92f2LSmu1MvgDrxHpkgKJ4jnHb3em2EkFfjQK9l3mawpDg%253D%253D');
+        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBWACn%252F2y29dMf0t1MUsCwe0RmoDJEOXfMjOEkeSXXkM5y24b4rRCdBRq4rbym8Wc5wgA4idJiyXWFZ6r%252FmN8nhyGQiTss3JlHiugpUP%252F00aYSx18VIFQju0A9TV0axSyl');
     lion.play();
     }
+
+    // usher
+
+    function playu1(){
+        let lion = new Audio("https://d274.cdn-me.net/p.php?s=eNoBOADH%252Fz4S%252FRYEjRapdDsw%252BHvPpsBAfOYIKXa4wSwIOHnQt%252BkBYzpMNUvYso8H08%252FIoPLAU2iZqn4lev%252FSznMbhA%253D%253D");
+    lion.play();
+    }
+    
+    function playu2(){
+        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBMADP%252F4t18jdYD%252FPjmTbAlDdu5U2BcHef7tXfR0C88BghKotEwI9Fl6WQgLLX4M8Pqrcm5nIuGgM%253D');
+    lion.play();
+    }
+    
+    function playu3(){
+        let lion = new Audio('https://d274.cdn-me.net/p.php?s=eNoBOADH%252F5%252FgxdC52qdYIgleXalYi%252FiIxfkljPafOUAq5E7frgf9JOg6QoJ14mbrZ6u873N6pobMTXtnDQx%252FgPkeHg%253D%253D');
+    lion.play();
+    }
+    
     
 
     
